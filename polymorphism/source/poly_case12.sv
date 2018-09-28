42fe180af9306017cd39b0063e6acf7d34d1e822
sharanbr <sharan.basappa@gmail.com>
2018-09-28 21:17:55 +0530

    Add files via upload

---
+++ polymorphism/source/poly_case12.sv
@@ -0,0 +1,28 @@
+module poly_case12;
+
+class BaseC;
+
+  virtual function func1;
+    $display ("func1 in BaseC");
+    func2;
+  endfunction
+
+  virtual function func2;
+    $display ("func2 in BaseC");
+  endfunction
+
+endclass: BaseC
+
+class DerivedC extends BaseC;
+
+endclass : DerivedC
+
+BaseC    P1 ;
+DerivedC P2 = new;
+
+initial begin
+  P1 = P2; 
+  P1.func1;
+end
+
+endmodule
---
+++ polymorphism/source/poly_case4.sv
@@ -0,0 +1,38 @@
+module poly_case4;
+
+class BaseC;
+
+  virtual function func1;
+    $display ("func1 in BaseC");
+    func2;
+  endfunction
+
+  function func2;
+    $display ("func2 in BaseC");
+  endfunction
+
+endclass: BaseC
+
+class DerivedC extends BaseC;
+
+  function func1;
+    $display ("func1 in DerivedC");
+    func2;
+  endfunction
+
+  function func2;
+    $display ("func2 in DerivedC");
+  endfunction
+
+  
+endclass : DerivedC
+
+BaseC    P1 ;
+DerivedC P2 = new;
+
+initial begin
+  P1 = P2; 
+  P1.func1;
+end
+
+endmodule

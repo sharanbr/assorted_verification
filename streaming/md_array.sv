1d5e7613a024a140ab7376350dcd8fdbd566de0b
sharanbr <sharan.basappa@gmail.com>
2018-12-08 14:27:12 +0530

    Update md_array.sv

--- streaming/md_array.sv
+++ streaming/md_array.sv
@@ -4,13 +4,15 @@ module x;
   bit [1:0] arr_1[0:1] = {2'b01, 2'b01};
   bit [3:0] arr_2;
   bit [3:0] arr_3;
+  bit arr_4[0:3];
   
   initial begin
     {>>{arr_2}} = arr_1;
     {<<{arr_3}} = arr_1;
+    {>>{arr_4}} = arr_1;
     
     $display ("%p ", arr_2);
     $display ("%p ", arr_3);
+    $display ("%p ", arr_4);
   end
-
 endmodule

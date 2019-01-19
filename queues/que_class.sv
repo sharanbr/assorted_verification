ea9f609861edb2ba8e7363e7df2645b94c253953
sharanbr <sharan.basappa@gmail.com>
2019-01-19 14:36:45 +0530

    Update que_class.sv

--- queues/que_class.sv
+++ queues/que_class.sv
@@ -1,22 +1,33 @@
 module x;
-
   class c1;
     int a,b;
   endclass
-  
+
   c1 c1_q[$];
-  
+
   function enqueue (c1 c1_par);
     c1_q.push_back(c1_par);
   endfunction
-  
+
   always begin
+    c1 c1_inst;
     for (int idx = 0; idx < 10; idx++) begin
-      c1 c1_inst;
       c1_inst = new;
       c1_inst.a = idx;
       c1_inst.b = idx*2;
+      #10ns;
     end
   end
-
+  
+  always begin
+    #10ns;
+    for (int idx = 0; idx < c1_q.size; idx++) begin
+      $display ("a %d b %d", c1_q[idx].a, c1_q[idx].b);
+    end
+  end
+  
+  initial begin
+    #1000ns;
+    $finish;
+  end
 endmodule
